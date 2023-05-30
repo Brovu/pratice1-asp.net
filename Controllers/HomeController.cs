783a6ffd8f59e16bd3b48c07f07b4314b7a2f8ba
Brovu <roy772003@gmail.com>
2023-05-30 23:20:25 +0700

    Practice1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice1.Model;
+
+namespace Practice1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View(new ListFriend().InitFriend());
+        }
+
+        public ActionResult SortAgeDesc()
+        {
+
+            return View(new ListFriend().InitFriend().OrderByDescending(m => m.age).ToList());
+
+        }
+
+        public ActionResult SortAgeAsc()
+        {
+            return View(new ListFriend().InitFriend().OrderBy(m => m.age).ToList());
+        }
+
+        public ActionResult SortMale()
+        {
+            return View(new ListFriend().InitFriend().Where(m => m.sex == "nam").ToList());
+        }
+
+        public ActionResult SortFemale()
+        {
+            return View(new ListFriend().InitFriend().Where(m => m.sex == "nữ").ToList());
+        }
+    }
+}
---
+++ Model/ListFriend.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice1.Model
+{
+    public class ListFriend
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string major { get; set; }
+
+        public string sex { get; set; }
+
+        public List<ListFriend> InitFriend()
+        {
+            var list = new List<ListFriend>();
+
+            list.Add(new ListFriend()
+            {
+                name = "Ngan",
+                age = 19,
+                major = "accountant",
+                sex = "nữ"
+        });
+
+            list.Add(new ListFriend()
+            {
+                name = "Minh",
+                age = 22,
+                major = "Hotel manager",
+                sex = "nam"
+            });
+
+            list.Add(new ListFriend()
+            {
+                name = "Tiên",
+                age = 18,
+                major = "Start-up",
+                sex = "nữ"
+            });
+
+            list.Add(new ListFriend()
+            {
+                name = "Thành",
+                age = 23,
+                major = "IT",
+                sex = "nam"
+            });
+
+            return list;
+        }
+    }
+}
